a46594e260a03ddd8391a5e36c0c5c85d95a2775
Logan Bunch <loganbunch@gmail.com>
2025-07-30 13:16:56 -0700

    Wed Jul 30 13:16:56 PDT 2025

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -180,9 +180,46 @@ return {
     ft = { 'markdown', 'mdx' },
     config = function()
       require('header-level').setup {
-        enabled = true,
         show_virtual_text = true, -- Show level as virtual text at end of line
-        update_events = { 'CursorMoved', 'CursorMovedI', 'BufEnter' },
+        virtual_text_position = 'fixed_corner',
+        inverted_colors = true,
+      }
+    end,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'auto',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff' },
+          -- lualine_c = { 'filename' },
+          lualine_c = {
+            'filename',
+            {
+              function()
+                local header_info = require('header-level').get_colored_header_level()
+                return header_info.text or ''
+              end,
+              color = function()
+                local header_info = require('header-level').get_colored_header_level()
+                return header_info.highlight and { fg = vim.g.markdown_header_level_hl } or nil
+              end,
+              cond = function()
+                return vim.bo.filetype == 'markdown' and vim.g.markdown_header_level ~= ''
+              end,
+            },
+          },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
       }
     end,
   },
